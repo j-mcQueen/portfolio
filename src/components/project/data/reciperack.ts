b611250b833bb48015b655ad6a3c5f298582c6bb
jack mcqueen <jdmcqueen44@gmail.com>
2024-07-24 16:34:14 -0400

    Write summary + motivations copy for reciperack project

--- src/components/project/data/reciperack.ts
+++ src/components/project/data/reciperack.ts
@@ -1,7 +1,12 @@
 export const reciperack = {
   title: "RECIPERACK",
-  summary: "",
-  motivations: ["", ""],
+  summary:
+    "In the application's current state, reciperack serves two purposes: to provide a hub to store, edit, and follow your favorite online recipes, and to provide a tool that permits the organisation of a user's food week using these collected recipes via a meal calendar.",
+  motivations: [
+    "The motivation to build reciperack stems from my love of cooking and frequent navigation of online recipes. Common in many of these types of  sites is a sub-optimal user experience that seem to favor pop-ups and advertisements which obstruct half the page and cause these bizarre loading patterns that lead to you losing your place on the page.",
+    "Equipped with this experience, I sensed an opportunity to build something that offered an alternative experience while solving a different problem: recipe decentralization! Often my favorite recipes are spread across so many different sites, and if I haven't saved them somewhere, some favorites can end up getting forgotten about - disappointing when you get a craving for those lost recipes.",
+    "With a little bit of copy-and-paste action (as you'll discover below), reciperack allows a user to centralize their favorite recipes all in one place and delivers an ad-free UI that formats instructions and ingredients to make following along easier. No longer will it take forever to find and navigate your most beloved recipes, and say goodbye to meal-time indecision!",
+  ],
   features: [
     { title: "ONLINE RECIPE CENTRALIZATION", paras: ["", "", "", ""] },
     { title: "WEEKLY MEAL PLANS", paras: ["", "", "", ""] },
